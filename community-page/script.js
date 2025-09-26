4bd392f38388d2257d01c8af4897e79b4629f385
kaitlinkl <kala2976@colorado.edu>
2025-09-25 19:29:18 -0600

    week 6 activity

--- community-page/script.js
+++ community-page/script.js
@@ -25,3 +25,32 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// cantos page stuff 
+let slideindex = 1;
+showSlides(slideindex);
+
+function plusSlides(n) {
+  showSlides(slideindex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideindex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("canto-slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideindex = 1}    
+  if (n < 1) {slideindex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideindex-1].style.display = "block";  
+  dots[slideindex-1].className += " active";
+}
+// end of cantos page stuff
